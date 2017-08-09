953cacaf9f66a4f4ae3fc91eed4baad3606b1aa5
RussellLuo <luopeng.he@gmail.com>
2017-08-09 22:33:58 +0800

    Add customizations example

---
+++ examples/customizations/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	v "github.com/RussellLuo/validating"
+)
+
+func validate(field v.Field) v.Errors {
+	switch t := field.ValuePtr.(type) {
+	case *map[string]time.Time:
+		if *t == nil {
+			return v.NewErrors(field.Name, v.ErrInvalid, "is empty")
+		}
+		return nil
+	default:
+		return v.NewErrors(field.Name, v.ErrUnsupported, "is unsupported")
+	}
+}
+
+type MyValidator struct{}
+
+func (mv *MyValidator) Validate(field v.Field) v.Errors {
+	return validate(field)
+}
+
+func main() {
+	var value map[string]time.Time
+
+	// do validation by funcValidator
+	funcValidator := v.FromFunc(func(field v.Field) v.Errors {
+		return validate(field)
+	})
+	errs := v.Validate(v.Schema{
+		v.F("value", &value): funcValidator,
+	})
+	fmt.Printf("errs from funcValidator: %+v\n", errs)
+
+	// do validation by structValidator
+	structValidator := &MyValidator{}
+	errs = v.Validate(v.Schema{
+		v.F("value", &value): structValidator,
+	})
+	fmt.Printf("errs from structValidator: %+v\n", errs)
+}
